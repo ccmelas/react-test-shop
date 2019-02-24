677d2d735955ebed8236eeed619248ac7f14b3ba
Chinedum Chiemela <chiemelachinedum@gmail.com>
2019-02-24 19:00:18 +0100

    Completes the product schema

---
+++ server/models/Product.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const slugs = require('slugs');
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Please provide a product name',
+        trim: true
+    },
+    slug: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: true
+    },
+    description: {
+        type: String,
+        required: 'Please provide a product description'
+    },
+    price: {},
+    category: {
+        type: String,
+        required: 'Please provide a product category'
+    },
+    image: {
+        type: String,
+        required: 'Please provide a product image',
+    },
+    color: {
+        type: String,
+        required: 'Please provide a product color'
+    }
+});
+
+ProductSchema.pre('save', async function(next) {
+    const product = this;
+
+    if (!product.isModified('name'))  return next();
+    
+    this.slug = slugs(this.name);
+
+    const slugRegExp = new RegExp(`^(${this.slug})((-[0-9]*?$))$`, 'i');
+
+    const productsWithSlug = await this.constructor.find({ slug: slugRegExp });
+
+    if (productsWithSlug.length) {
+        this.slug = `${this.slug}-${productsWithSlug.length + 1}`;
+    }
+    
+    next();
+});
+
+module.exports = model('Product', ProductSchema);
