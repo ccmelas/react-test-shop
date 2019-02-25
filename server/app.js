585a89f86ccee00358abc904fe6614812e6932df
Chinedum Chiemela <chiemelachinedum@gmail.com>
2019-02-25 19:01:23 +0100

    Fixes typo

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use('/api/', apiRoutes);
 
 // Serve the react application
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'../build/index.html'));
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
 });
 
 // Registers handler for when routes are not found
