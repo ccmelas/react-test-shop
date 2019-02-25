a7670789e4208299de58d3fc4f1dd02c68fc7240
Chinedum Chiemela <chiemelachinedum@gmail.com>
2019-02-25 18:50:48 +0100

    Updates react host

--- src/services/host.js
+++ src/services/host.js
@@ -1,1 +1,2 @@
-export const host = `http://localhost:7777/api/v1`;
+// export const host = `http://localhost:7777/api/v1`;
+export const host = `https://shrouded-citadel-16454.herokuapp.com/api/v1`;
