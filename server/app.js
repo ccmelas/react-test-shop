b493fcd29a8162fadc5a36706a7d1816e54b2f58
Chinedum Chiemela <chiemelachinedum@gmail.com>
2019-02-26 10:38:45 +0100

    Removes cors middleware

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const logger = require('morgan');
 const path = require('path');
 const apiRoutes = require('./routes');
 const ErrorHandler = require('./handlers/ErrorHandler');
 
 const app = express();
-
-app.use(cors());
 
 // Set up logging
 app.use(logger('dev'));
